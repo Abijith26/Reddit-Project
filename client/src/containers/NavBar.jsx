0eb3e250a8dfcd8382355d7aed345125033c65f7
Abijith26 <abimurugan.c@gmail.com>
2025-01-06 17:33:50 +0530

    Completed the Reddit Logo Part

--- client/src/containers/NavBar.jsx
+++ client/src/containers/NavBar.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import Logo from "../components/Logo";
 
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <div>
+      <Logo />
+    </div>
+  );
 }
